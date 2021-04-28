15520a20ea0d9cec4fe9724c886c915bc0e0756c
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-28 15:21:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function superbowlWin(array) {
 
 const record = [
   {year: "2018", result: "L"},
-  {year: "2017", result: "N"},
+  {year: "2017", result: "W"},
   {year: "2016", result: "N/A"}
   //...
 ]
