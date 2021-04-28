46383b0beb8fd7ec62c5a2c4af75639293bc5f7d
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-28 15:16:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function superbowlWin(array) {
   return array.find(e => e.result === "W").year
 }
 
-// const record = [
-//   {year: "2018", result: "L"},
-//   {year: "2017", result: "W"},
-//   {year: "2016", result: "N/A"}
-//   //...
-// ]
-// console.log(superbowlWin(record))
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "L"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+console.log(superbowlWin(record))
